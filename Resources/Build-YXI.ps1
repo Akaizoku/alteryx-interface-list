94c378708a2f9e7b76093f8b1d1feacaeb0dc1cf
Florian Carrier <Akaizoku@users.noreply.github.com>
2025-12-19 14:10:51 +0100

    Restructuration
    Update repository structure

--- Resources/Build-YXI.ps1
+++ Resources/Build-YXI.ps1
@@ -11,25 +11,29 @@ $Author             = "Florian CARRIER"         # Author
 $Version            = "1.0.0"                   # Version number
 $CategoryName       = "Interface"               # Tool category
 $Description        = "This macro enables users to use a list of values to populate an interface tool easily. It automatically transposes the values from rows to columns to match the expected format of the interface tools, and provides options to deduplicate and order values."
-$Icon               = ".\Resources\icon.png"    # Package icon
+$Icon               = "icon.png"                # Package icon
 $CompressionLevel   = "Optimal"                 # YXI compression level
 
 # ------------------------------------------------------------------------------
 # * Prepare package content
 # ------------------------------------------------------------------------------
 # Create temporary package folder
-$Package = ".\YXI"
+$Root    = Split-Path -Parent $PSScriptRoot
+$Package = Join-Path -Path $Root -ChildPath "YXI"
 New-Item -Path $Package -ItemType "Directory" -Force | Out-Null
-# Clean-up sample outputs
-try {Remove-Item -Path ".\Samples\en\Macros\Output" -Recurse -Force -ErrorAction "Stop"} catch {}
 # Select files to include in the package
-Copy-Item -Path ".\$Name" -Destination $Package -Exclude "*.bak" -Recurse -Force
+$ExcludeDirs  = @(".git", "Resources", "YXI")
+$ExcludeExact = @("README.md", "LICENSE")
+$ExcludeLike  = @("*.bak", ".git*", "*.yxi")
+Get-ChildItem -Path $Root -Force |
+  Where-Object {
+    $ItemName = $PSItem.Name
+    $ItemName -notin $ExcludeDirs -and
+    $ItemName -notin $ExcludeExact -and
+    -not ($ExcludeLike | Where-Object { $ItemName -like $PSItem })
+  } |
+  Copy-Item -Destination $Package -Recurse -Force
 Copy-Item -Path $Icon -Destination "$Package\icon.png" -Force
-Copy-Item -Path ".\Samples" -Destination $Package -Exclude "*.bak" -Recurse -Force
-# Add localized samples
-foreach ($Language in @("de", "es", "fr", "it", "ja", "pt", "xx", "zh")) {
-    Copy-Item -Path ".\Samples\en" -Destination "$Package\Samples\$Language" -Recurse -Force
-}
 # Resolve package path
 $Path = Resolve-Path -Path $Package
 
